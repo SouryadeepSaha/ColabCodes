10494d0466df06172d3e2b26cdb9058893c1ecaf
Souryadeep Saha <sourya.saha@fau.de>
2023-05-05 11:18:02 +0200

    trying out cmake and make

---
+++ ClionProject/cmake-build-test/CMakeFiles/ClionProject.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/ClionProject.dir/main.cpp.obj"
+  "CMakeFiles/ClionProject.dir/main.cpp.obj.d"
+  "ClionProject.exe"
+  "ClionProject.exe.manifest"
+  "ClionProject.pdb"
+  "libClionProject.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/ClionProject.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ ClionProject/cmake-build-test/source/CMakeFiles/Source.dir/cmake_clean.cmake
@@ -0,0 +1,5 @@
+
+# Per-language clean rules from dependency scanning.
+foreach(lang )
+  include(CMakeFiles/Source.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
